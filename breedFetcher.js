95901ef4268987a451b8021b848bd54287e9c561
Danek <danek.fill@outlook.com>
2022-07-07 18:06:30 +0000

    completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,26 @@ const request = require('request');
 
 const fetchSite = (url) => {
   request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  if (error !== null){
+    console.log('error:', error); // Print the error if one occurred
+  };
+
+  if('statusCode' !== 200){
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('Website not found, check API url settings.')
+  }
   // console.log('body:', body); // Print the HTML for the page.
 
   const data = JSON.parse(body);
-  console.log(data);
-
+  //if nothing found
+ try{
+   console.log(data[0].description);
+ } 
+ catch {
+  console.log("Cat breed not found, please check spelling and try agin.")
+ }
 });
-}
+};
 
 let url = "https://api.thecatapi.com/v1/breeds/search?q="
 let breed = process.argv.slice(2);
